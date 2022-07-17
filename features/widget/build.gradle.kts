df0e04bce773f6f7ff0934884138352d8df2abe0
nicholasdoglio <NicholasDoglio@Gmail.com>
2022-07-16 23:19:39 -0400

    Use decoupled Compose compiler version

--- features/widget/build.gradle.kts
+++ features/widget/build.gradle.kts
@@ -52,7 +52,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.get()
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 
     compileOptions {
