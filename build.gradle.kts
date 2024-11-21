058c68b2d32d0bf2834b2072a12e464c1d1e56b8
nicholasdoglio <NicholasDoglio@Gmail.com>
2024-11-21 07:06:15 -0500

    Upgrade to Gradle 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,4 @@ plugins {
 
 
 // https://docs.gradle.org/8.9/userguide/gradle_daemon.html#daemon_jvm_criteria
-tasks.updateDaemonJvm.configure { jvmVersion = JavaVersion.toVersion(libs.versions.jdk.get()) }
+tasks.updateDaemonJvm.configure { jvmVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get())) }
